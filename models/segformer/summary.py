48b1932c574b2c8637f8a9b532637e08ab66b514
valhassan <victor.alhassan@canada.ca>
2024-10-16 19:11:48 -0400

    Refactor SegFormer model and update summary script

--- models/segformer/summary.py
+++ models/segformer/summary.py
@@ -1,11 +1,14 @@
+import torch
 import segformer
 from torchinfo import summary
 
 
 print("Segformer MIT-B5")
 model = segformer.SegFormer("mit_b5", in_channels=3, classes=5)
-batch_size = 16
-summary(model, input_size=(batch_size, 3, 512, 512))
+batch_size = 8
+x = torch.rand(batch_size, 3, 512, 512)
+out = model(x)
+# summary(model, input_data=[x])
 
 
 if __name__ == '__main__':
